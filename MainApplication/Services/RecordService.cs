95d1a1d5f1037560df2de177a078944e5a195166
Unknown <178570188@qq.com>
2017-12-19 17:29:55 +0800

    RecordingsPage UI初步
    把wjw的recordservice拉了进来（虽然没用到）

---
+++ MainApplication/Services/RecordService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTV6.Models;
+using iTV6.Models.Stations;
+using iTV6.Utils;
+using Windows.Storage.Streams;
+using Windows.Storage;
+using System.Collections;
+using System.Net;
+
+namespace iTV6.Services
+{
+    class RecordService
+    {
+        public async void download(Uri RequestUri)
+        {
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            StorageFile sampleFile = await storageFolder.CreateFileAsync("sample.txt", CreationCollisionOption.ReplaceExisting);
+            //获取m3u8文件
+            Windows.Web.Http.HttpClient http = new Windows.Web.Http.HttpClient();
+            var buffer = await http.GetBufferAsync(RequestUri);
+            //处理m3u8文件，缓存至text中
+            DataReader TempData = Windows.Storage.Streams.DataReader.FromBuffer(buffer);
+            string text = TempData.ReadString(buffer.Length);
+            //存储所有当前.ts文件的名字
+            ArrayList tsContent = TSposition(text);
+            string tempTs;
+            string uri = RequestUri.ToString();
+            int hlsPos = uri.IndexOf("hls");
+            Uri tempTsUri;
+            foreach (object i in tsContent)
+            {
+                tempTs = i.ToString();
+                tempTsUri = new Uri(uri.Substring(0, hlsPos + ("hls/").Length) + tempTs);
+                http = new Windows.Web.Http.HttpClient();
+
+
+                buffer = await http.GetBufferAsync(tempTsUri);
+                sampleFile = await storageFolder.CreateFileAsync("sample.ts", CreationCollisionOption.ReplaceExisting);
+                await FileIO.WriteBufferAsync(sampleFile, buffer);
+
+            }
+
+
+
+
+
+            //StorageFile sampleFile = await storageFolder.GetFileAsync("sample.txt");
+
+
+
+
+        }
+
+        //返回.m3u8文件中所有.ts文件的名字
+        //目前版本中使用-11的操作，这是固定.ts文件的长度确定的，之后可以更改
+        public ArrayList TSposition(string text)
+        {
+            ArrayList newlist = new ArrayList();
+            int i = 0;
+            int tempPos = 0;
+            while (i < text.Length)
+            {
+                tempPos = text.IndexOf(".ts", i);
+                if (tempPos == -1)
+                    break;
+                newlist.Add(text.Substring(tempPos - 11, 11 + (".ts").Length));
+                i = tempPos + 3;
+            }
+            return newlist;
+        }
+    }
+}
---
+++ MainApplication/Views/RecordDialog.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using iTV6.Services;
+using iTV6.Models;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“内容对话框”项模板
+
+namespace iTV6.Views
+{
+    public sealed partial class RecordDialog : ContentDialog
+    {
+        public RecordDialog()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            var channel = ChannelPicker.SelectedItem as Channel;
+            var startTime = StartDatePicker.Date.Value.Date.Add(StartTimePicker.Time);
+            var endTime = EndDatePicker.Date.Value.Date.Add(EndTimePicker.Time);
+            //var source = await TelevisionService.Instance.GetPlaybackSource(channel, startTime, endTime);
+            //NavigationService.ShellNavigation.Navigate<PlayerPage>(new Tuple<Uri, string>(source, "回看：" + channel.Name));
+            this.Hide();
+        }
+
+        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            this.Hide();
+        }
+
+        private async void ContentDialog_Opened(ContentDialog sender, ContentDialogOpenedEventArgs args)
+        {
+            var pbStation = TelevisionService.Instance.TelevisionStations.First(station => station is IPlaybackStation) as ITelevisionStation;
+            ChannelPicker.ItemsSource = (await pbStation.GetChannelList()).Select(program => program.ProgramInfo.Channel).Distinct();
+            ChannelPicker.SelectedIndex = 0;
+            StartDatePicker.Date = EndDatePicker.Date = DateTimeOffset.Now;
+        }
+    }
+}
