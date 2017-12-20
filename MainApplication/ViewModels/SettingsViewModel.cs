faeaa82b630837664be7074f18ff836f01132f76
Unknown <178570188@qq.com>
2017-12-20 08:54:49 +0800

    目前所有设置已经可以保存
    当然现在还是没有什么用

--- MainApplication/ViewModels/SettingsViewModel.cs
+++ MainApplication/ViewModels/SettingsViewModel.cs
@@ -13,28 +13,78 @@ namespace iTV6.ViewModels
 {
     class SettingsViewModel : ViewModelBase
     {
-        public List<SourceRecord> MediaSources { get; } = new List<SourceRecord>();
-        private SourceRecord _PoriorSource = null;
-        public SourceRecord PriorSource
+        //默认视频源
+        public List<string> MediaSources { get; } = new List<string>();
+        private string _poriorSource = null;
+        public string PriorSource
         {
-            get { return _PoriorSource; }
+            get { return _poriorSource; }
             set
             {
-                Set(ref _PoriorSource, value);
+                Set(ref _poriorSource, value);
+                Settings.SetValue("PriorSource", _poriorSource);
             }
         }
-
-        private string _FilePath="...";//录播保存地址
+        //录播文件存储地址
+        private string _filePath="...";//录播保存地址
         public string FilePath
         {
-            get { return _FilePath; }
-            set { Set(ref _FilePath, value); }
+            get { return _filePath; }
+            set {
+                Set(ref _filePath, value);
+                Settings.SetValue("FilePath", _filePath);
+            }
         }
-
+        //主题
+        public List<string> ThemeList = new List<string>();
+        private string _selectedTheme = null;
+        public string SelectedTheme {
+            get { return _selectedTheme;}
+            set {
+                Set(ref _selectedTheme,value);
+                Settings.SetValue("Theme", _selectedTheme);
+            }
+        }
+        //是否发送桌面通知
+        private bool _sendDesktopNotifications = false;
+        public bool SendDesktopNotifications
+        {
+            get { return _sendDesktopNotifications;}
+            set {
+                Set(ref _sendDesktopNotifications, value);
+                Settings.SetValue("SendDesktopNotifications", _sendDesktopNotifications);
+            }
+        }
+        //是否使用系统日历
+        private bool _useSystemCalender = false;
+        public bool UseSystemCalender
+        {
+            get { return _useSystemCalender; }
+            set
+            {
+                Set(ref _useSystemCalender, value);
+                Settings.SetValue("UseSystemCalender", _useSystemCalender);
+            }
+        }
+        //构造函数
         public SettingsViewModel()
         {
-            MediaSources = TelevisionService.Instance.AvaliablePrograms.First().MediaSources;//这里的source好像不大对啊，有各种后缀
-            PriorSource = MediaSources.First();
+            //MediaSources = TelevisionService.Instance.AvaliablePrograms.First().MediaSources;//这里的source好像不大对啊，有各种后缀
+            //PriorSource = MediaSources.First();
+            MediaSources.Add("清华"); MediaSources.Add("中国农大"); MediaSources.Add("东北大学"); MediaSources.Add("北邮人");
+            ThemeList.Add("浅色"); ThemeList.Add("深色"); ThemeList.Add("蓝色");
+            if (Settings.ContainsKey("PriorSource"))//读取设置：默认视频源
+                PriorSource = Settings.GetValue("PriorSource").ToString();
+            if (Settings.ContainsKey("FilePath"))//读取设置：存储路径
+                FilePath = Settings.GetValue("FilePath").ToString();
+            if (Settings.ContainsKey("Theme"))//读取设置：主题
+                SelectedTheme = Settings.GetValue("Theme").ToString();
+            else
+                SelectedTheme = ThemeList.First();
+            if (Settings.ContainsKey("SendDesktopNotifications"))//读取设置：是否发送桌面通知
+                SendDesktopNotifications = (bool)Settings.GetValue("SendDesktopNotifications");
+            if (Settings.ContainsKey("UseSystemCalender"))//读取设置：是否使用系统日历
+                UseSystemCalender = (bool)Settings.GetValue("UseSystemCalender");
         }
 
         public async void BrowseButtonTapped()//点了“浏览”之后，选取文件夹
