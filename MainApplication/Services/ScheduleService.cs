bdf5270235efd30853afd4f94994329bddadf07f
Jacob C <cmpute@qq.com>
2017-12-27 21:21:22 +0800

    清理代码和备注

--- MainApplication/Services/ScheduleService.cs
+++ MainApplication/Services/ScheduleService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
@@ -10,15 +11,22 @@ namespace iTV6.Services
 {
     public class ScheduleService
     {
-        private ScheduleService()
-        {
-            SetScheduleForSingleDistrict("cctv");
-        }
-        private static Dictionary<string, List<Models.Program>> Schedule;
-        private static Dictionary<string, string> DistrictToCode;
-        private static int[] timestamp = { 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22 };
+        private ScheduleService() { }
+        private Dictionary<string, string> _districtCodes = new Dictionary<string, string>();
+        private Dictionary<string, List<Models.Program>> _schedule = new Dictionary<string, List<Models.Program>>();
+        public IReadOnlyDictionary<string, List<Models.Program>> Schedule => _schedule;
+        public IReadOnlyDictionary<string, string> DistrictToCode => _districtCodes;
 
-        public static Uri GetUri(string code, int time, int DoW = -1)
+        public static ScheduleService Instance { get; } = new ScheduleService();
+
+        /// <summary>
+        /// 计算节目单页面的网址
+        /// </summary>
+        /// <param name="code">区域代码</param>
+        /// <param name="time">小时</param>
+        /// <param name="DoW">星期几, -1代表今天</param>
+        /// <returns></returns>
+        private static Uri GetUri(string code, int time, int DoW = -1)
         {
             if(DoW == -1)
             {
@@ -32,48 +40,53 @@ namespace iTV6.Services
             return UriTemp; 
         }
 
-        public static void SetScheduleForSingleDistrict(string code,int Dow = -1)
+        /// <summary>
+        /// 获取指定区域的当天节目单
+        /// </summary>
+        /// <param name="code">区域代码</param>
+        /// <param name="Dow">第几天</param>
+        public async Task SetScheduleForDistrict(string code, int Dow = -1)
         {
-            foreach(int time in timestamp)
+            List<Task> tasks = new List<Task>();
+            for (int time = 0; time < 24; time += 2)
             {
                 Uri UriTemp = GetUri(code, time, Dow);
-                SetScheduleFromSinglePage(UriTemp);
+                await SetScheduleFromPage(UriTemp);
+                //tasks.Add(SetScheduleFromPage(UriTemp));
             }
+            // await Task.WhenAll(tasks);
         }
 
-        public static async void SetScheduleFromSinglePage(Uri href)
+        /// <summary>
+        /// 根据电视猫页面链接获取节目单
+        /// </summary>
+        /// <param name="href">节目单链接</param>
+        private async Task SetScheduleFromPage(Uri href)
         {
             HttpClient client = new HttpClient();
             var response = await client.GetByteArrayAsync(href);
-            string result = SuperEncoding.UTF8.GetString(response);
+            string result = Encoding.UTF8.GetString(response);
             HtmlDocument document = new HtmlDocument();
             document.LoadHtml(result);
 
-
             HtmlNode rootNode = document.DocumentNode;
             HtmlNodeCollection channelNode = rootNode.SelectNodes("/body[1]/div[@class='page-content clear']/div[@class='timeline clear']/table[@class='timetable']/table[@class='timetable']");
-            HtmlNode channel = null;
-            foreach (HtmlNode ch in channelNode)
+            foreach (HtmlNode channel in channelNode)
             {
-                channel = HtmlNode.CreateNode(ch.OuterHtml);
-                HtmlNode chNode = channel.SelectSingleNode("tr[1]/td[@class='tdchn']");
-                HtmlNodeCollection proCollect = channel.SelectNodes("tr[1]/td[@class='tdpro']");
+                HtmlNode chNode = channel.SelectSingleNode("./tr[1]/td[@class='tdchn']");
+                HtmlNodeCollection proCollect = channel.SelectNodes("./tr[1]/td[@class='tdpro']");
                 string chName = chNode.InnerText;
                 string uniqueKey = SuperEncoding.GetSpellCode(chName);
-                Models.Channel chn = null;
-                chn = Models.Channel.GetChannel(uniqueKey, chName, Models.Channel.GetChannelTypeByName(chName));
+                Models.Channel chn = Models.Channel.GetChannel(uniqueKey, chName);
                 List<Models.Program> pro = new List<Models.Program>();
-                if (!Schedule.ContainsKey(chn.UniqueId))
-                {
-                    Schedule.Add(chn.UniqueId, pro);
-                }
+                if (!_schedule.ContainsKey(chn.UniqueId))
+                    _schedule.Add(chn.UniqueId, pro);
 
-                foreach (HtmlNode pg in proCollect)
+                foreach (HtmlNode prog in proCollect)
                 {
-                    HtmlNode prog = HtmlNode.CreateNode(pg.OuterHtml);
-                    HtmlNode progInfo = prog.SelectSingleNode("/td[1]");
-                    string progName = progInfo.FirstChild.InnerText.Replace("&nbsp;", "");
-                    string startTime = progInfo.LastChild.InnerText.Replace("&nbsp;", "");
+                    HtmlNode progInfo = prog.SelectSingleNode("./td[1]");
+                    string progName = WebUtility.HtmlDecode(prog.FirstChild.InnerText).Trim();
+                    string startTime = WebUtility.HtmlDecode(prog.LastChild.InnerText).Trim();
 
                     if (string.Equals(startTime, ".."))
                     {
@@ -85,57 +98,50 @@ namespace iTV6.Services
                         DateTime starttime = Convert.ToDateTime(startTime);
                         pgm.Name = progName;
                         pgm.StartTime = starttime;
-                        Schedule[chn.UniqueId].Add(pgm);
+                        _schedule[chn.UniqueId].Add(pgm);
                     }
                 }
             }
         }
-
-
         
-        public async void SetDistrictToCode()
+        /// <summary>
+        /// 获取频道区域与代码关系表
+        /// </summary>
+        public async Task SetDistrictToCode()
         {
             HttpClient client = new HttpClient();
-            var response = await client.GetByteArrayAsync("http://www.tvmao.com/program/duration/cctv/w7-h16.html");
+            var response = await client.GetByteArrayAsync("http://www.tvmao.com/program");
             string result = SuperEncoding.UTF8.GetString(response);
             HtmlDocument document = new HtmlDocument();
             document.LoadHtml(result);
 
             HtmlNode rootNode = document.DocumentNode;
-            DistrictToCode.Add("央视", "cctv");
-            DistrictToCode.Add("卫视", "satellite");
-            DistrictToCode.Add("数字付费", "digital");
-            DistrictToCode.Add("香港", "honkong");
-            DistrictToCode.Add("澳门", "macau");
-            DistrictToCode.Add("台湾", "taiwan");
-            DistrictToCode.Add("境外", "foreign");
+            _districtCodes.Add("央视", "cctv");
+            _districtCodes.Add("卫视", "satellite");
+            _districtCodes.Add("数字付费", "digital");
+            _districtCodes.Add("香港", "honkong");
+            _districtCodes.Add("澳门", "macau");
+            _districtCodes.Add("台湾", "taiwan");
+            _districtCodes.Add("境外", "foreign");
 
-            HtmlNodeCollection NodeList = rootNode.SelectNodes("/body[1]/div[@class = 'pgnav_wrap']/div[@class='lev2 clear']/form[@class='lt ml10']/select[@name='prov']");
-            NodeList = NodeList[0].ChildNodes;
-            int id = 0;
+            HtmlNodeCollection NodeList = rootNode.SelectNodes("/body/div[@class='pgnav_wrap']/div[@class='lev2 clear']/form[@class='lt ml10']/select[@name='prov']");
             string disName = null;
             string disCode = null;
-            foreach (HtmlNode nd in NodeList)
+            foreach (HtmlNode node in NodeList[0].ChildNodes)
             {
-                HtmlNode node = HtmlNode.CreateNode(nd.OuterHtml);
-
                 if (node.Attributes.Contains("value"))
                 {
                     disCode = node.Attributes["value"].Value;
                     continue;
                 }
                 disName = node.InnerText;
-                if (disName == null || disCode == null || disName == "" || disCode == "" || disCode == "0")
+                if (string.IsNullOrWhiteSpace(disName) || string.IsNullOrWhiteSpace(disCode) || disCode == "0")
                 {
                     continue;
                 }
-                DistrictToCode.Add(disName, disCode);
-                id += 1;
-                DistrictToCode.Count();
+                _districtCodes.Add(disName, disCode);
             }
-
         }
-         
     }
 }
     
