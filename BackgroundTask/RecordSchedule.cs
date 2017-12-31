bdee0a18d7981a711dde7ee5fb8ae2f56fb49e11
Jacob C <cmpute@qq.com>
2017-12-31 17:47:12 +0800

    添加后台任务工程

---
+++ BackgroundTask/RecordSchedule.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTV6.Background
+{
+    /// <summary>
+    /// 在设置中保存的录播计划
+    /// </summary>
+    public class RecordSchedule
+    {
+        private string _playlistUri;
+        private string _uriPrefix;
+        /// <summary>
+        /// 直播文件(.m3u8/.flv）的Uri地址
+        /// </summary>
+        public string PlaylistUri
+        {
+            get { return _playlistUri; }
+            set
+            {
+                _playlistUri = value;
+                _uriPrefix = _playlistUri.Remove(_playlistUri.LastIndexOf('/'));
+            }
+        }
+
+        /// <summary>
+        /// 开始录制的时间
+        /// </summary>
+        public DateTime StartTime { get; set; }
+
+        /// <summary>
+        /// 计划录播的时间
+        /// </summary>
+        public TimeSpan ScheduledTime { get; set; }
+
+        /// <summary>
+        /// 录播状态
+        /// </summary>
+        public ScheduleStatus Status { get; set; }
+    }
+
+    public enum ScheduleStatus
+    {
+        Scheduled, // 计划中，未开始
+        Downloading, // 正在下载中
+        Completed, // 下载完毕
+        Failed, // 下载失败
+        Terminated // 用户终止
+    }
+}
---
+++ MainApplication/Services/BackgroundService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace iTV6.Services
+{
+    /// <summary>
+    /// 新建后台任务
+    /// </summary>
+    public class BackgroundService
+    {
+        public static async Task<BackgroundTaskRegistration> RegisterBackgroundTask(
+            Type taskEntryPoint, string taskName, IBackgroundTrigger trigger, IBackgroundCondition condition)
+        {
+            var status = await BackgroundExecutionManager.RequestAccessAsync();
+            if (status == BackgroundAccessStatus.Unspecified || status == BackgroundAccessStatus.Denied)
+                return null;
+
+            // 取消已有任务
+            foreach (var cur in BackgroundTaskRegistration.AllTasks)
+                if (cur.Value.Name == taskName)
+                    cur.Value.Unregister(true);
+
+            // 建立任务对象
+            var builder = new BackgroundTaskBuilder
+            {
+                Name = taskName,
+                TaskEntryPoint = taskEntryPoint.FullName
+            };
+
+            builder.SetTrigger(trigger);
+
+            if (condition != null)
+                builder.AddCondition(condition);
+            
+            BackgroundTaskRegistration task = builder.Register();
+
+            Debug.WriteLine($"成功注册 {taskName} 后台任务");
+
+            return task;
+        }
+    }
+}
